ca00854f42ce26601b515acbb9149ae9ebdc7336
Joseph <josephkim0703@gmail.com>
2024-08-13 12:12:41 -0700

    script

---
+++ dungeon game/src/Game/blank.jsx
@@ -0,0 +1,68 @@
+
+function rename(){
+
+
+
+        return(
+            <h1>
+So how did you find out about the ruins most people only believe as a fairytail
+
+Um i just heard someone talking about it at a bar
+
+wait so you know nothing about the ruins of alexnadria and why theres people searching for it
+
+um not really
+
+oh my god well let me tell you why you see every 5000 years a huge island emerge from the sea
+bringing choas with it narutral disaster which caused famines and wipes the planet clean its like
+the world is cleansing itself
+
+the worse thing that came out of the island was the death plaque
+
+what the death plaque was from the island??! 
+
+yes a virus from the island made it to mainland spreading like wild fire causes millions to die but this was no ordinary virus it 
+mutated the victims turning them into savage barbaric monsters no race was safe even the powerful high elves could not do anything
+
+but the people that saved us were non other then humans you see human dont have the organ that can store or process mana so human can not use
+magic but due to the virus some human instead of mutating into monster instead evolved and gain supernatural abilities surpassing magic
+
+you see magic is created through storing mana inside one self and using is as a sort of fuel
+
+oh so kind of like gun powder
+
+exactly but these humans instead of storeing the mana and outputting magic instead was like as if there body produced the magic itself
+its as if these powers were apart of them.
+
+these human are known as the 12 pillars there powers passed though generations though not all the bloodlines survived and only 4
+of the 12 still are still known to be alive
+
+wait so king Aine is one of those 12 pillar kids
+
+yes he is a descendant of one of them.
+
+but just cause the virus was handle did not mean the disaster was over 
+
+before the world could be wiped out 1 powerful high elf mages as well as 2 of the pillars sacrifaced themselves and sealed the island and stopped it from
+submerging into the ocean so the cycle doesnt repeat itself
+
+The only way onto the island is to find the teleportaion circle left by the mage only one remain
+and it is said its hidden deep in a dungeon filled with traps making sure no one can reach it
+
+people rumor that the reason the island bring such chaos is becuase its protecting somthing pricless on the island others say the mages isnt dead 
+but sealed it away and faked his death to keep the island to himself.
+
+and to think you were going to search for alexandira with no clue in the world is well really funny
+
+Thats why I have you now partner so where to well from my research the plaque first started in the southern region in a small village
+its located near a huge canyon that could be where the circle is.
+
+Wait are you talking about Lyrindell thats where I'm from and now that you speak of it I remeber there being a huge cave system i used
+to explore as a kid that could be the dungeon. 
+            </h1>
+
+        )
+        
+    
+
+}
